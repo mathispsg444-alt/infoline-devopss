9b77f31f5b0ed7a408be928ef0729c3c080c6b01
Kilian <mathispsg444@gmail.com>
2026-02-19 14:58:04 +0100

    Add AWS provider and resources for EKS and Lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,18 @@
+# Configuration du provider AWS
 provider "aws" {
   region = "eu-west-1"
+}
+
+# Création d'un cluster Kubernetes EKS
+resource "aws_eks_cluster" "infoline_cluster" {
+  name     = "infoline-cluster"
+  role_arn = "arn:aws:iam::123456789012:role/eks-role"
+}
+
+# Fonction Lambda pour authentification
+resource "aws_lambda_function" "auth_function" {
+  function_name = "infoline-auth"
+  runtime       = "java17"
+  handler       = "com.infoline.AuthHandler"
+  role          = "arn:aws:iam::123456789012:role/lambda-role"
 }
