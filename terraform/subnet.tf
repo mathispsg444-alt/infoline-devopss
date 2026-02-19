c698d71a81fdbe8d3524a1fb677c14cc181f5c40
Kilian <mathispsg444@gmail.com>
2026-02-19 15:46:30 +0100

    Add public subnet resource in subnet.tf

---
+++ terraform/subnet.tf
@@ -0,0 +1,11 @@
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.infoline_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "infoline-public-subnet"
+  }
+}
