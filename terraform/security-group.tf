3f120bd61dcb3b2ca5f79886b2ea5e84cbabd334
Kilian <mathispsg444@gmail.com>
2026-02-19 15:47:57 +0100

    Create security group for HTTP and SSH access
    Defines a security group allowing HTTP, SSH, and custom port access.

---
+++ terraform/security-group.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "infoline_sg" {
+  name        = "infoline-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.infoline_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
